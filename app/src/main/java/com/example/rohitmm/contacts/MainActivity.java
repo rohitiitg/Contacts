56f03e895c4a8eceeecc44a6e6634ce11f9763c0
rohitiitg <42818264+rohitiitg@users.noreply.github.com>
2019-04-22 03:01:55 +0530

    commented

--- app/src/main/java/com/example/rohitmm/contacts/MainActivity.java
+++ app/src/main/java/com/example/rohitmm/contacts/MainActivity.java
@@ -19,7 +19,7 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {                   
     private int Read_Contact_Permission = 1;
     ListView l1;
     ImageView imageView;
@@ -31,25 +31,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_CONTACTS},Read_Contact_Permission);
+        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_CONTACTS},Read_Contact_Permission);       // Requesting the permission to read contacts
         l1 = (ListView) findViewById(R.id.list_contacts);
 
-        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME); // setting cursor to read name, phoneid and and number of contact 
         startManagingCursor(cursor);
 
-        String[] from = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER , ContactsContract.CommonDataKinds.Phone._ID};
-        int [] to = {android.R.id.text1,android.R.id.text2};
+        String[] from = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER , ContactsContract.CommonDataKinds.Phone._ID};   // Creating required string for the adapter
+        int [] to = {android.R.id.text1,android.R.id.text2};            // making corresponding int array for listview
 
-        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,cursor,from,to);
-        l1.setAdapter(simpleCursorAdapter);
-        l1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,cursor,from,to);         // setting the adapter for listview
+        l1.setAdapter(simpleCursorAdapter);                                                                                                 // setting adapter in listview
+        l1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);    // setting appropriate layout of listview
 
-        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {                   // setting reaction when clicked on an item
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getApplicationContext(),Details.class);
+                Intent i = new Intent(getApplicationContext(),Details.class);               // getting intent of Details class
 
-                i.putExtra("id",id);                                            //action is to start new activity which shows full view of the image.
+                i.putExtra("id",id);                                            //action is to start new activity which shows details of contact
                 startActivity(i);
             }
         });
