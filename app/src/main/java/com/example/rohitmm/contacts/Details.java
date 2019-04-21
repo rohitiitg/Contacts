c1218caf3df1571a07d19f73e249f685b80d72ce
rohitiitg <42818264+rohitiitg@users.noreply.github.com>
2019-04-22 03:17:09 +0530

    commented

--- app/src/main/java/com/example/rohitmm/contacts/Details.java
+++ app/src/main/java/com/example/rohitmm/contacts/Details.java
@@ -29,73 +29,73 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);             // Instatiation of various views in the activity
         TextView text_name = (TextView) findViewById(R.id.text_name);
         TextView text_number = (TextView) findViewById(R.id.text_number);
         TextView text_email = (TextView) findViewById(R.id.text_email);
-        Intent intent = getIntent();
-        long id = intent.getExtras().getLong("id");
+        Intent intent = getIntent();                                               // getting intent of current activity
+        long id = intent.getExtras().getLong("id");                                // getting the phoneid of the clicked contact
         Cursor c = getContentResolver().query(
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                new String[]{
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,                 // creating cursor to read the datils as name, number,photoidand contact id of the contact clicked 
+                new String[]{                                                      // creating corresponding string to read the datils as mentioned in above line
                         ContactsContract.CommonDataKinds.Phone.NUMBER,
                         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
                         ContactsContract.CommonDataKinds.Phone.PHOTO_ID,
                         ContactsContract.CommonDataKinds.Phone.CONTACT_ID
-                }, ContactsContract.CommonDataKinds.Phone._ID + "=?", new String[]{Long.toString(id)}, null);
+                }, ContactsContract.CommonDataKinds.Phone._ID + "=?", new String[]{Long.toString(id)}, null); //setting constraint to matching phone id of contact
 
         String contactid = new String();
 
         if (c != null) {
 
             if (c.moveToFirst()) {
-                String number = c.getString(0);
-                String name = c.getString(1);
-                int photoId = c.getInt(2);
-                contactid = c.getString(3);
+                String number = c.getString(0);                 //retrieving number of the contact
+                String name = c.getString(1);                   // retrieving name of the contact
+                int photoId = c.getInt(2);                      // getting photoid of contact
+                contactid = c.getString(3);                     // getting contact id of the contact( this is differnt from the phoneid
 
-                text_name.setText("Name       :  "+name);
+                text_name.setText("Name       :  "+name);       // viewing name and number in text view
                 text_number.setText("Number   :  "+number);
 
-                Bitmap bitmap = queryContactImage(photoId);
-                imageView.setImageBitmap(bitmap);
+                Bitmap bitmap = queryContactImage(photoId);     // getting the contact image by query contact function
+                imageView.setImageBitmap(bitmap);               // setting image to imageview
 
             }
-            c.close();
+            c.close();                                          // closing the cursor
 
         }
 
-        Cursor ce = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                new String[]{
+        Cursor ce = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,          // creating cursor for reading the email of the contact
+                new String[]{                                                                               // crating corresponding string for retrieving email address
                         ContactsContract.CommonDataKinds.Email.DATA
-                },ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[] {contactid}, null);
+                },ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[] {contactid}, null);    // setting constraint to desired contact id
         if(ce!=null && ce.moveToFirst()){
-            String emailAdd = ce.getString(0);
+            String emailAdd = ce.getString(0);                      // getting email id of contact as string
             text_email.setText("Email        :  "+emailAdd);
         }
-        ce.close();
+        ce.close();                                                 // closing the email cursor.
 
 
     }
 
-    private Bitmap queryContactImage(int imageDataRow) {
-        Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI, new String[]{
+    private Bitmap queryContactImage(int imageDataRow) {                //this fuction is used to retrieve the image as bitmap when photo id is known
+        Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI, new String[]{          // crating corresponding cursor to read the image 
                 ContactsContract.CommonDataKinds.Photo.PHOTO
-        }, ContactsContract.Data._ID + "=?", new String[]{
+        }, ContactsContract.Data._ID + "=?", new String[]{                  // setting constraint to photo id
                 Integer.toString(imageDataRow)
         }, null);
         byte[] imageBytes = null;
         if (c != null) {
             if (c.moveToFirst()) {
-                imageBytes = c.getBlob(0);
+                imageBytes = c.getBlob(0);                  // getting the blob of the image
             }
-            c.close();
+            c.close();          // closing the cursor
         }
 
         if (imageBytes != null) {
-            return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length); // reeturn the image blob in form of bitmap
         } else {
-            return null;
+            return null;    // return null if no image found
         }
 
     }
